ddf9fabe70ca9859195807e290a4ad1f6a82d4d0
snakebang <709432917@qq.com>
2016-05-31 21:06:24 +0800

    ...

--- IE-alert.js
+++ IE-alert.js
@@ -1,6 +1,6 @@
         window.onload = function() {
-            var text1 = document.createTextNode("很抱歉您的IE浏览器版本过低，请更新您的IE浏览器或者下载现代浏览器，如："),
-                text2 = document.createTextNode("Chrome谷歌浏览器 "),
+            var text1 = document.createTextNode("很抱歉您的IE浏览器已经过时，请更新您的IE浏览器或者下载其他浏览器，如："),
+                text2 = document.createTextNode("360极速浏览器 "),
                 text3 = document.createTextNode("Firefox火狐浏览器 "),
                 text4 = document.createTextNode("Opera浏览器 "),
                 text5 = document.createTextNode("了解更多..."),
@@ -30,8 +30,8 @@
             a2.style.textDecoration = "none";
             a3.style.textDecoration = "none";
             a4.style.textDecoration = "none";
-            a1.href = "http://www.google.cn/intl/zh-CN/chrome/browser/desktop/index.html";
-            a2.href = "http://www.firefox.com.cn/";
+            a1.href = "http://chrome.360.cn/";
+            a2.href = "http://www.firefox.com.cn//";
             a3.href = "http://www.opera.com/zh-cn";
             a4.href = "http://rj.baidu.com/soft/lists/3";
             h1.align = "center";
